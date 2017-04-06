c1c51bd9e3581ee8b961ebb290fc4450d06bc4e8
dittonjs <jditton.atomic@gmail.com>
2017-04-06 12:36:31 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# eslint-plugin-atomic-jolt
+
+Custom eslint rules for Atomic Jolt
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-atomic-jolt`:
+
+```
+$ npm install eslint-plugin-atomic-jolt --save-dev
+```
+
+**Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-atomic-jolt` globally.
+
+## Usage
+
+Add `atomic-jolt` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "atomic-jolt"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "atomic-jolt/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+`no-splat-props // disallow {...this.props} to child react components`
---
+++ tests/lib/rules/no-splat-props.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-splat-props"),
+    RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+var parserOptions = {
+  ecmaVersion: 6,
+  ecmaFeatures: {
+    jsx: true
+  }
+};
+
+ruleTester.run("no-splat-props", rule, {
+  valid: [
+    {code: "<div style={{}}>Hello world</div>", parserOptions: parserOptions},
+    {code: "<div {...someOtherObject}>Hello world</div>", parserOptions: parserOptions}
+  ],
+  invalid: [
+    {
+      code: "<div {...this.props}>Hello world</div>",
+      parserOptions: parserOptions,
+      errors: [{
+        message: 'Do not splat the props to child components, instead pass each prop individualy or create an object that has only the values that the child needs from probs and splat that object.',
+        type: "JSXOpeningElement"
+      }]
+    }, {
+      code: "<div {...props}>Hello world</div>",
+      parserOptions: parserOptions,
+      errors: [{
+        message: 'Do not splat the props to child components, instead pass each prop individualy or create an object that has only the values that the child needs from probs and splat that object.',
+        type: "JSXOpeningElement"
+      }]
+    }
+  ]
+});
